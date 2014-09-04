139f8a23c62fb6da4a694e63f400a18ef5346c02
Stian Fredsted Dahl <stian.fredsted@gmail.com>
2014-09-04 11:09:38 +0200

    Lecture 3 added

---
+++ Lecture3/app/src/main/java/dk/stiandahl/lecture3/FirstActivity.java
@@ -0,0 +1,49 @@
+package dk.stiandahl.lecture3;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class FirstActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.first, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sendBtn_onClick(View view) {
+        //Start SecondActivity
+        EditText inputText = (EditText) findViewById(R.id.textInput);
+
+        Intent intent = new Intent();
+        intent.putExtra("text", inputText.getText().toString());
+        intent.setClass(this, SecondActivity.class);
+        startActivity(intent);
+    }
+}
